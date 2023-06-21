a5ed6544e787a55cc9a23591ab726a1eea447847
pratik2820 <130150715+pratik2820@users.noreply.github.com>
2023-06-21 15:56:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 //your JS code here. If required.
+const time = document.getElementById("timer");
+// const date = new Date();
+// const timer = date.toLocaleTimeString();
+// setInterval(updateTimer, 1000);
+
+const date = new Date();
+const currentDate = date.toDateString();
+
+function updateTimer() {
+      const date = new Date();
+      const timer = date.toLocaleTimeString();
+      
+      // document.getElementById("timer").textContent = timer;
+    }
+
+    // Call updateTimer function every second
+    setInterval(updateTimer, 1000);
+time.innerText = currentDate;
+// time.innerText = timer;
